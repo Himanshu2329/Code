fe9bd3f2153030142c750f61bbea129944089e14
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-03-23 23:54:35 +0530

    New Directory

---
+++ BasicRecursion/BasicRecursion.java
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+               package BasicRecursion;
+=======
+// package BasicRecursion;
+>>>>>>> 7b94128 (Done)
+
+public class BasicRecursion {
+    public static void main(String[] args) { 
+        // int n=1;
+        int y=50;
+        // getElements(n,y);   
+        getElementsY(y);
+        // System.out.println(res);                             
+    }
+    private static void getElementsY(int y) {
+        // TODO Auto-generated method stub
+        if(y<1){          
+            return;
+        }
+        System.out.println(y);
+        getElementsY(y-1);
+        // throw new UnsupportedOperationException("Unimplemented method 'getElementsY'");
+    }
+    static void getElements(int n, int y){
+        if(n>y){
+            return;
+        }
+        System.out.println(n);
+      getElements(n+1,y);
+        
+    } 
+}
---
+++ BasicRecursion/PrintNtimeWord.java
@@ -0,0 +1,27 @@
+// package BasicRecursion;
+
+import java.util.*;
+
+public class PrintNtimeWord {
+    public static void main(String[] args) { 
+        int n=3;
+        // List<Integer> List = 
+         GetElement(n);
+        // System.out.println(List);
+    }
+
+    private static void GetElement(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if(n<1){
+            return ;
+        }
+        list.add(n);
+        GetElement(n-1);
+        // return list;
+        System.out.println(list);
+        // TODO Auto-generated method stub
+
+        // throw new UnsupportedOperationException("Unimplemented method 'GetElement'");
+    }
+    
+}

ba26a5358a6e50f63261e89caae970699e00d051
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-03-24 22:59:09 +0530

    n to 1 to n done

--- BasicRecursion/PrintNumber.java
+++ BasicRecursion/PrintNumber.java
@@ -1,6 +1,4 @@
-/**
- * PrintNumber
- */
+
 public class PrintNumber {
 
     public static void main(String[] args) {
@@ -12,13 +10,18 @@ public class PrintNumber {
             return;
         }
         // 1 to N
-        getNum(n-1);
-        System.out.print(n);
-
-
-
+        // getNum(n-1);
+        // System.out.print(n); 
+        
+        
         // N to 1
         // System.out.print(n);
         // getNum(n-1);
+        
+        //  n to 1 to n
+        System.out.print(n); 
+        getNum(n-1);
+        System.out.print(n); 
+
     }
 }

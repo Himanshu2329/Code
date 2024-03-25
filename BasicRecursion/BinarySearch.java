06c0041747d63a4ca9117b2b9ba30bbecc728d16
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-03-25 21:53:42 +0530

    Update BinarySearch.java

--- BasicRecursion/BinarySearch.java
+++ BasicRecursion/BinarySearch.java
@@ -7,7 +7,7 @@ public class BinarySearch {
     }
     static int search(int []arr,int target, int s, int e){
         if(s>e){
-            return -1;  
+            return -1;   
         }
         int m=s+(e-s)/2;
         if(arr[m]==target){
